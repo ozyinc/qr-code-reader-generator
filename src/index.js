6cc4d62ea216b272cfda4a8ac1db04c8e9f68ca2
Ozan İncesulu <ozyinc@users.noreply.github.com>
2021-05-25 16:42:43 +0200

    chore: move to Parcel from that weirdness

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
-import './main.css';
-import './bootstrap.min.css';
+import './main.scss';
 import { Elm } from './Main.elm';
-import registerServiceWorker from './registerServiceWorker';
 
 Elm.Main.init({
   node: document.getElementById('root')
 });
 
-registerServiceWorker();
